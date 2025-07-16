0e8f9d33425f2b0a08f724c634db1c9ee56cc183
leonardo <lopez.leonardo@uabc.edu.mx>
2025-07-16 14:01:45 -0700

    [Basic Calculator Android]: Added BasicOperations class to the logic.

---
+++ BasicCalculator/app/src/main/java/com/example/basiccalculator/BasicOperations.java
@@ -0,0 +1,21 @@
+package com.example.basiccalculator;
+
+public class BasicOperations {
+
+    public BasicOperations (){
+
+    }
+    public int add(int number1, int number2) { return number1 + number2; }
+
+    public int multiplication(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int subtraction(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public float division(int number1, int number2) {
+        return number1 / number2;
+    }
+}
