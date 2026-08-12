5173cfd8aa480935aca81644588b814d44a2140a
leonardo <lopez.leonardo@uabc.edu.mx>
2026-08-12 14:55:47 -0700

    [Kotlin Android]: In complement of "Hello World", I added set edittext when onClick is pressed.

--- HelloWordKotlin/app/src/main/java/com/project/hellowordkotlin/MainActivity.kt
+++ HelloWordKotlin/app/src/main/java/com/project/hellowordkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.project.hellowordkotlin
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,17 +23,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*fun clickOnMe(view: View){
-        // val URLText = findViewById<View?>(R.id.ClickOnMeButton) as Button
-        Toast.makeText(applicationContext, "Hello World Button!!", Toast.LENGTH_SHORT).show()
-
-    }*/
     fun clickOnMe(view: View) {
         Toast.makeText(applicationContext, "Hello World Button!!", Toast.LENGTH_SHORT).show()
     }
 
     fun onRegister(view: View) {
-        Toast.makeText(applicationContext, "Register Button!!", Toast.LENGTH_SHORT).show()
+        val myTextView = findViewById<TextView>(R.id.emailId)
+        val myEmailTextView = findViewById<TextView>(R.id.emailTextID)
+
+        val emailAdress:String = myTextView.text.toString()
+        Toast.makeText(applicationContext, "Register Button!!\n" + emailAdress, Toast.LENGTH_SHORT).show()
+
+        myEmailTextView.text = myTextView.text.toString()
     }
 
     fun onLogin(view: View) {
