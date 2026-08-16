388c5422e6ff634865073c08da0ed4a8dc773616
Leo <leo@dev.com>
2026-08-15 22:04:48 -0700

    [Calculator Kotlin]: Added the basic methods, some XML properties and some validations for the calculator.

--- CalculatorKotlin/app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
+++ CalculatorKotlin/app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculatorkotlin
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,40 +24,63 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onAdd(view: View) {
+    fun onCalculatorOperation(view: View){
 
-    }
-
-    fun onSubtraction(view: View) {
-
-    }
-
-    fun onMultiplication(view: View) {
-
-    }
-
-    fun onDivision(view: View) {
-
+        if(view.id == R.id.addId){
+            operations.setAddFlag(true);
+        } else if (view.id == R.id.subsId) {
+            operations.setSubtractionFlag(true);
+        } else if (view.id == R.id.multiplicationId) {
+            operations.setMultiplicationFlag(true);
+        } else if (view.id == R.id.divisionId) {
+            operations.setDivisionFlag(true);
+        }
     }
 
     fun onResult(view: View) {
         val value1 = findViewById<EditText>(R.id.value1)
         val value2 = findViewById<EditText>(R.id.value2)
+        val resultId = findViewById<TextView>(R.id.resultId)
 
         val number1: Double = value1.text.toString().toDouble()
         val number2: Double = value2.text.toString().toDouble()
 
         val addResult = operations.add(number1,number2)
 
+        resultId.setText(addResult.toString())
+
+        if(operations.getAddFlag()){
+            val addResult = operations.add(number1,number2)
+            resultId.setText(addResult.toString())
+        } else if (operations.getSubtractionFlag()) {
+            val addResult = operations.subtraction(number1,number2)
+            resultId.setText(addResult.toString())
+        } else if (operations.getMultiplicationFlag()) {
+            val addResult = operations.multiplication(number1,number2)
+            resultId.setText(addResult.toString())
+        } else if (operations.getDivisionFlag()) {
+            val addResult = operations.division(number1,number2)
+            resultId.setText(addResult.toString())
+        }
+
+        onClear();
+
     }
 
-    fun onClear(view: View) {
+    fun onClear() {
         val value1 = findViewById<EditText>(R.id.value1)
         val value2 = findViewById<EditText>(R.id.value2)
         val symbol = findViewById<EditText>(R.id.symbolId)
+        val resultId = findViewById<TextView>(R.id.resultId)
 
         value1.setText("")
         value2.setText("")
         symbol.setText("")
+        resultId.setText("")
+
+        operations.setAddFlag(false);
+        operations.setSubtractionFlag(false);
+        operations.setMultiplicationFlag(false);
+        operations.setDivisionFlag(false);
     }
 }
--- CalculatorKotlin/app/src/main/java/com/example/calculatorkotlin/Operation.kt
+++ CalculatorKotlin/app/src/main/java/com/example/calculatorkotlin/Operation.kt
@@ -1,6 +1,14 @@
 package com.example.calculatorkotlin
 
+import android.R
+import android.view.View
+
 class Operation() {
+
+    var isAdd: Boolean = false
+    var isSubtraction: Boolean = false
+    var isMultiplication: Boolean = false
+    var isDivision: Boolean = false
 
     fun add(a: Double, b: Double): Double {
         return a+b
@@ -10,11 +18,43 @@ class Operation() {
         return a-b
     }
 
-    fun divisio(a: Double, b: Double): Double {
+    fun division(a: Double, b: Double): Double {
         return a/b
     }
 
     fun multiplication(a: Double, b: Double): Double {
         return a*b
+    }
+
+    fun setAddFlag(flag: Boolean) {
+        isAdd = flag;
+    }
+
+    fun setSubtractionFlag(flag: Boolean ) {
+        isSubtraction = flag
+    }
+
+    fun setMultiplicationFlag(flag: Boolean ) {
+        isMultiplication = flag
+    }
+
+    fun setDivisionFlag(flag: Boolean ) {
+        isDivision = flag
+    }
+
+    fun getAddFlag() : Boolean {
+        return isAdd
+    }
+
+    fun getSubtractionFlag() : Boolean {
+        return isSubtraction
+    }
+
+    fun getMultiplicationFlag() : Boolean {
+        return isMultiplication
+    }
+
+    fun getDivisionFlag() : Boolean {
+        return isDivision
     }
 }
