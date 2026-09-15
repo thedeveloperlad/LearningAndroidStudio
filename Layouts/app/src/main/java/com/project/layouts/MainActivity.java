cf0173ae3afdeb79ce050524a6b97daabb7b9bab
leonardo <lopez.leonardo@uabc.edu.mx>
2026-09-15 16:26:25 -0700

    [LAYOUTS]: Added Layouts project with their activities to learn how to use RelativeLayout, LinearLayout, and GridLayout on XML.

---
+++ Layouts/app/src/main/java/com/project/layouts/MainActivity.java
@@ -0,0 +1,41 @@
+package com.project.layouts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void relativeLayoutExample(View view){
+        Intent intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void linearLayoutExample(View view){
+        Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void gridLayoutExample(View view){
+        Intent intent = new Intent(MainActivity.this, GridLayoutActivity.class);
+        startActivity(intent);
+    }
+}
